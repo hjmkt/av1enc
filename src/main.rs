2807c9865cb4ba809f24bb2f67e34030b82159a6
hjmkt <hjmkt3126@gmail.com>
2018-05-14 05:50:56 +0900

    Add option parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate colored;
 mod option;
+use option::*;
+use colored::*;
+use std::process;
 
 fn main() {
-    println!("Hello, world!");
+
+    println!("{} version {} Copyright (c) 2018-2018 {}", env!("CARGO_PKG_NAME").to_string(), env!("CARGO_PKG_VERSION").to_string(), env!("CARGO_PKG_AUTHORS"));
+
+    let mut config: Config = Config::default();
+
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
+    if args.len() > 0 {
+        CommandOption::init_config(&mut config);
+        if let Err(e) = CommandOption::parse(args, &mut config) {
+            println!("{}: failed to parse command-line arguments: {}", "error".red(), e);
+            process::exit(0);
+        }
+        if let Err(e) = CommandOption::validate_config(&config) {
+            println!("{}: invalid configuration: {}", "error".red(), e);
+            process::exit(0);
+        }
+    }
 }
--- src/option.rs
+++ src/option.rs
@@ -1,64 +1,190 @@
 extern crate num;
 
-use num::rational::Ratio;
+use self::num::rational::Ratio;
+use std::str::FromStr;
+use std::process;
 
-struct Config {
-    frame_width: i16,
-    frame_height: i16,
-    framerate: Ratio<i16>,
+pub struct Config {
+    input: Option<String>,
+    output: Option<String>,
+    frame_width: Option<u16>,
+    frame_height: Option<u16>,
+    framerate: Option<Ratio<u16>>,
 }
 
-struct CommandOption {
-    name: str,
-    short_name: Option(str),
-    usage: str,
-    default: Option<str>,
-    parser: fn(str, &mut Config) -> Result<i32, str>
+impl Default for Config {
+    fn default() -> Config { Config {
+        input: None,
+        output: None,
+        frame_width: None,
+        frame_height: None,
+        framerate: None
+    }}
 }
 
-impl CommandOption {
-    fn parse_tuple<T: FromStr>(arg: str, sep: char, len: i32, tuple: &mut [T]) -> Result<i32, str> {
-        let v = arg.split(sep).collect();
+pub struct CommandOption {
+    name: &'static str,
+    short_name: Option<&'static str>,
+    usage: &'static str,
+    default: Option<&'static str>,
+    parser: fn(&Vec<String>, &mut Config) -> Result<Vec<String>, String>,
+    validator: fn(&Config) -> Result<i32, String>
+}
+
+impl<'a> CommandOption {
+    fn parse_tuple<T: FromStr>(arg: &'a str, sep: char, len: usize, tuple: &mut [&mut Option<T>]) -> Result<i32, String> {
+        let v : Vec<&str> = arg.split(sep).collect();
         if v.len() == len {
-            for i in 0..(v.len()-1) {
-                match T::from_str(v[i]) {
-                    Ok(res) => tuple[i] = res,
-                    Err(e) => return Err("Invalid argument")
-                }
+            for i in 0..v.len() {
+                if let Ok(res) = T::from_str(v[i]) { *tuple[i] = Some(res); }
+                else { return Err("wrong format".to_string()); }
             }
             Ok(0)
         }
-        else{
-            Err("Invalid argument")
-        }
+        else{ Err("wrong format".to_string()) }
     }
 }
 
-static options: Vec<CommandOption> = [
-    CommandOption {
-        name: "size",
-        short_name: "s",
-        usage: "video resolution (WxH)",
-        default: None,
-        parser: | arg: str, config: &mut Config | -> Result<i32, str> {
-            CommandOption::parse_tuple::<u16>(arg, 'x', 2, [config.frame_width, config.frame_height])
+lazy_static! {
+    static ref OPTIONS : Vec<CommandOption> = vec![
+        CommandOption {
+            name: "help",
+            short_name: Some("h"),
+            usage: "show help message",
+            default: None,
+            parser: | _args: &Vec<String>, _config: &mut Config | -> Result<Vec<String>, String> {
+                CommandOption::show_help_and_exit();
+                Ok(vec![])
+            },
+            validator: | _config | -> Result<i32, String>  { Ok(0) }
+        },
+        CommandOption {
+            name: "input",
+            short_name: Some("i"),
+            usage: "input path or 'stdin' for standard input",
+            default: None,
+            parser: | args: &Vec<String>, config: &mut Config | -> Result<Vec<String>, String> {
+                config.input = Some(args[0].clone());
+                Ok(vec![])
+            },
+            validator: | _config | -> Result<i32, String>  { Ok(0) }
+        },
+        CommandOption {
+            name: "output",
+            short_name: Some("o"),
+            usage: "output path or 'stdout' for standard output",
+            default: None,
+            parser: | args: &Vec<String>, config: &mut Config | -> Result<Vec<String>, String> {
+                config.output = Some(args[0].clone());
+                Ok(vec![])
+            },
+            validator: | _config | -> Result<i32, String>  { Ok(0) }
+        },
+        CommandOption {
+            name: "size",
+            short_name: Some("s"),
+            usage: "video resolution (WxH)",
+            default: None,
+            parser: | args: &Vec<String>, config: &mut Config | -> Result<Vec<String>, String> {
+                if let Err(e) = CommandOption::parse_tuple::<u16>(&args[0], 'x', 2, &mut [&mut config.frame_width, &mut config.frame_height]) { Err(e) }
+                else { Ok(args[1..].to_vec()) }
+            },
+            validator: | config: &Config | -> Result<i32, String>  {
+                if let (Some(w), Some(h)) = (config.frame_width, config.frame_height) {
+                    if w>0 && w<8192 && h>0 && h<8192 { Ok(0) }
+                    else { Err("invalid video resolution specified.".to_string()) }
+                }
+                else { Err("Video resolution not specified.".to_string()) }
+            }
+        },
+        CommandOption {
+            name: "framerate",
+            short_name: Some("r"),
+            usage: "video frame rate (N/D, or N for N/1)",
+            default: Some("30/1"),
+            parser: | args: &Vec<String>, config: &mut Config | -> Result<Vec<String>, String> {
+                if let Ok(n) = u16::from_str(&args[0]) {
+                    config.framerate = Some(Ratio::new(n, 1));
+                    Ok(args[1..].to_vec())
+                }
+                else {
+                    let (mut denom, mut numer) = (None, None);
+                    if let Err(e) = CommandOption::parse_tuple::<u16>(&args[0], '/', 2, &mut [&mut numer, &mut denom]) { Err(e) }
+                    else {
+                        config.framerate = Some(Ratio::new(numer.unwrap(), denom.unwrap()));
+                        Ok(vec![])
+                    }
+                }
+            },
+            validator: | config: &Config | -> Result<i32, String>  {
+                if let Some(r) = config.framerate {
+                    if r.numer() > &0 && r.denom() > &0 { Ok(0) }
+                    else { Err("invalid video framerate specified.".to_string()) }
+                }
+                else { Err("video framerate not specified.".to_string()) }
+            }
         }
-    },
-    CommandOption {
-        name: "framerate",
-        short_name: "r",
-        usage: "video frame rate (N/D, or N for N/1)",
-        default: None,
-        parser: | arg: str, config: &mut Config | -> Result<i32, str> {
-            match u16::from_str(arg) {
-                Ok(n) => {
-                    config.framerate = Ratio(n, 1);
-                    Ok(0)
-                }
-                Err(_) => {
-                    CommandOption::parse_tuple::<u16>(arg, '/', 2, [config.frame_width, config.frame_height])
-                }
+    ];
+}
+
+impl CommandOption {
+    fn set_option_by_name(name: &str, args: Vec<String>, config: &mut Config) -> Result<Vec<String>, String> {
+        for opt in OPTIONS.iter() {
+            if opt.name == name { return match (opt.parser)(&args, config) {
+                Ok(v) => Ok(v),
+                Err(e) => Err(format!("invalid argument '{}' for option '--{}': {}", args[0], name, e))
+            }}
+        }
+        Err(format!("option '--{}' not found.", name))
+    }
+
+    fn set_option_by_short_name(name: &str, args: Vec<String>, config: &mut Config) -> Result<Vec<String>, String> {
+        for opt in OPTIONS.iter() {
+            if opt.short_name == Some(name) { return match (opt.parser)(&args, config) {
+                Ok(v) => Ok(v),
+                Err(e) => Err(format!("invalid argument '{}' for option '-{}': {}", args[0], name, e))
+            }}
+        }
+        Err(format!("option '-{}' not found.", name))
+    }
+
+    pub fn init_config(config: &mut Config) {
+        for opt in OPTIONS.iter() {
+            if let Some(o) = opt.default {
+                if let Err(e) = (opt.parser)(&[o.to_string()].to_vec(), config) { assert!(false, e) }
             }
         }
     }
-];
+
+    pub fn validate_config(config: &Config) -> Result<i32, String> {
+        for opt in OPTIONS.iter() {
+            if let Err(e) = (opt.validator)(config) { return Err(e) }
+        }
+        Ok(0)
+    }
+
+    fn show_help_and_exit() {
+        for opt in OPTIONS.iter() {
+            if let Some(sn) = opt.short_name { println!("--{} (-{}): {}", opt.name, sn, opt.usage) }
+            else { println!("--{}: {}", opt.name, opt.usage); }
+        }
+        process::exit(0);
+    }
+
+    pub fn parse(args: Vec<String>, config: &mut Config) -> Result<Vec<String>, String> {
+        if args.len() == 0 { Ok(vec![]) }
+        else {
+            let arg = &args[0];
+            if &arg[0..1] == "-" {
+                match {
+                    if &arg[1..2] == "-" { CommandOption::set_option_by_name(&arg[2..], args[1..].to_vec(), config) }
+                    else { CommandOption::set_option_by_short_name(&arg[1..], args[1..].to_vec(), config) }
+                } {
+                    Ok(v) => CommandOption::parse(v, config),
+                    Err(e) => Err(e)
+                }
+            }
+            else{ return Err("option name must start with '-'.".to_string()) }
+        }
+    }
+}
